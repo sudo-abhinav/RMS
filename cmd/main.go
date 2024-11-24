f6663edb3e51ff7da36418ad3e56d2bdcd04a53a
sudo-abhinav <akimabhinav@gmail.com>
2024-11-24 16:42:06 +0530

    minor changes

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	_ "github.com/lib/pq"
 	"github.com/sirupsen/logrus"
 	"github.com/sudo-abhinav/rms/database"
 	"github.com/sudo-abhinav/rms/routes"
+	"log/slog"
+	_ "log/slog"
 	"net/http"
 	"os"
 	"os/signal"
@@ -17,16 +18,22 @@ import (
 const shutDownTime = 10 * time.Second
 
 func main() {
+	//TODO :-  implementing logger
+	logger := slog.New(slog.NewJSONHandler(os.Stderr, nil))
+	slog.SetDefault(logger)
+
+	logger.Info("JSONHandler Example", "Content", "Logging in JSON format")
+
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
-	//fmt.Println(os.Getenv("DB_HOST"), "hello")
+
 	server := routes.SetupRoutes()
-	fmt.Printf(
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASS"))
+	//fmt.Printf(
+	//	os.Getenv("DB_HOST"),
+	//	os.Getenv("DB_PORT"),
+	//	os.Getenv("DB_NAME"),
+	//	os.Getenv("DB_USER"),
+	//	os.Getenv("DB_PASS"))
 
 	if err := database.ConnectAndMigrate(
 		os.Getenv("DB_HOST"),
@@ -35,25 +42,26 @@ func main() {
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASS"),
 		database.SSLModeDisable); err != nil {
-		logrus.Panicf("Failed to initialize and migrate database with error = %+v", err)
+		logger.Error("Failed to initialize and migrate database with error = %+v", err)
 	}
-	logrus.Print("migration successfully..")
+	logger.Info("migration successfully..")
 
 	go func() {
 		if err := server.RUN(":3000"); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			logrus.Panicf("Failed to run server with error: %+v", err)
+			logger.Warn("Failed to run server with error: %+v", err)
 		}
 	}()
-	logrus.Print("Server started at :3000")
+	logger.Info("Server started at :3000")
 	<-done
 
-	logrus.Info("shutting down server")
+	logger.Info("Server Shutdown", "Content", "shutting down server")
+	//logrus.Info("shutting down server")
 
 	if err := database.ShutDownDB(); err != nil {
 		logrus.WithError(err).Error("failed to close database connection")
 	}
 
 	if err := server.Shutdown(shutDownTime); err != nil {
-		logrus.WithError(err).Panic("failed to gracefully shutdown server")
+		logger.With(err).Warn("failed to gracefully shutdown server")
 	}
 }
